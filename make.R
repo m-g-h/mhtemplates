c3d24e618c0f3be009258be29b2d2abad51eee44
m-g-h <haasmartin@live.de>
2021-09-15 09:49:21 +0200

    Updated gitignore in latex template

--- make.R
+++ make.R
@@ -1,3 +1,4 @@
+library(magrittr)
 
 # Save original wd for later reset
 originalwd <- getwd()
@@ -5,8 +6,19 @@ originalwd <- getwd()
 # Copy files of latex template as .zip to /inst
 setwd("templates/latex/")
 
-zip("../../inst/templates/latex",
-    files = list.files(all.files = T,recursive = T))
+delete_files = list.files(all.files = T,
+                          recursive = T) %>%
+  stringr::str_subset("\\.frontmatter|\\.gitignore|\\.preamble|\\.bib|figures|main\\.tex|symbols\\.tex|tables",
+                      negate = T)
+
+template_zip_file_tex = "../../inst/templates/latex.zip"
+
+file.remove(delete_files, template_zip_file_tex)
+
+zip(zipfile = template_zip_file_tex,
+    files = list.files(all.files = T,
+                       recursive = T)
+)
 
 setwd(originalwd)
 
@@ -14,7 +26,11 @@ setwd(originalwd)
 
 setwd("templates/xaringan/")
 
-zip("../../inst/templates/xaringan",
+template_zip_file_xaringan = "../../inst/templates/xaringan"
+
+file.remove(template_zip_file_xaringan)
+
+zip(zipfile = template_zip_file_xaringan,
     files = list.files(all.files = T,recursive = T))
 
 setwd(originalwd)
